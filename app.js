79d91c55e3c760a3ace3cc91c9f19937cfc7ad25
Daniel Calder <dancalder98@gmail.com>
2023-01-17 18:05:37 -0500

    Adding requirements

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const uuid = require('uuid')
 const app = express()
+const cors = require('cors')
+const session = require('express-session')
+
+const FileStore = require('session-file-store')(session)
+const bodyParser = require('body-parser');
+
+const passport = require('passport')
+const localStrategy = require('passport-local')
+
+
 const port = 3000
 
 app.get('/', (req,res)=>{
