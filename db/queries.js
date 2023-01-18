62e58ec9dfdadbfbad758729e9e6b454c81dedf5
Daniel Calder <dancalder98@gmail.com>
2023-01-18 13:21:42 -0500

    Added GET queries for each database

--- db/queries.js
+++ db/queries.js
@@ -7,6 +7,19 @@ const getOrderByID = 'SELECT * FROM orders WHERE order_id = $1';
 const getProducts = 'SELECT * FROM product';
 // Fetch a specific product by ID
 const getProductById = 'SELECT * FROM product WHERE product_id = $1';
+// Fetch all carts
+const getCarts = 'SELECT * FROM cart';
+//Fetch specific Cart
+const getCartByID = 'SELECT * FROM cart WHERE cart_id = $1';
+//Fetch Cart Items
+const getCartItems = 'SELECT * FROM cart_item';
+//Fetch specific Cart Items
+const getCartItemByID = 'SELECT * FROM cart_item WHERE cart_item_id = $1';
+//Fetch Users
+const getUsers = 'SELECT * FROM user_info';
+//Fetch Specific User
+const getUserByID = 'SELECT * FROM user_info WHERE user_id = $1';
+
 
 
 // Export queries
@@ -14,5 +27,11 @@ module.exports = {
     getOrders,
     getOrderByID,
     getProducts,
-    getProductById
+    getProductById,
+    getCarts,
+    getCartByID,
+    getCartItems,
+    getCartItemByID,
+    getUsers,
+    getUserByID
 };
